e9226e23e7fe49a9ee734a2fa6a3b45f875a0d8d
William Pimentel Vieira <pimentelvieira@brq.com>
2017-12-14 16:50:25 -0200

    Commit

---
+++ javaweb-netgifx/src/br/com/netgifx/util/JPAUtil.java
@@ -0,0 +1,14 @@
+package br.com.netgifx.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+
+	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("javaweb_netgifx");
+	
+	public static EntityManager getEntityManager() {
+		return emf.createEntityManager();
+	}
+}
